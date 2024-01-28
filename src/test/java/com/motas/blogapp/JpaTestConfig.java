0243b317a558be85633cc4d737686bff1207ee52
ronak <ronkasingh2708@gmail.com>
2024-01-28 18:04:39 +0530

    First Commit

--- src/test/java/com/motas/blogapp/JpaTestConfig.java
+++ src/test/java/com/motas/blogapp/JpaTestConfig.java
@@ -19,6 +19,7 @@ public class JpaTestConfig {
         dataSource.setDriverClassName("org.h2.Driver");
         dataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
 
+
         return dataSource;
     }
 
